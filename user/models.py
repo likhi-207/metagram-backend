e987b105a74c42fdce7c13a869ed21e505c2aea6
likhi-207 <likhitharudraraju207@gmail.com>
2025-07-16 07:13:00 +0530

    metagramBackend

---
+++ user/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=10)
+    userName=models.CharField(max_length=20,unique=True)
+    dob=models.CharField(max_length=8)
+    mobile=models.IntegerField(max_length=10)
+
+
+    def _str_(self):
+        return self.name
---
+++ user/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import User
+from .userSerializer import UserSerializer
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view(['POST'])
+def login(request):
+    serializer=UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        realUserData=serializer.data
+        print(realUserData)
+        return Response(realUserData)
